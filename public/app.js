e0e4d5c8df15b62376eb32c8b2c70d3ea2b41333
Zach Sharpe <ziggi24@gmail.com>
2021-01-12 21:52:21 -0800

    working v1 with shareed state across clients

--- public/app.js
+++ public/app.js
@@ -1,1 +1,50 @@
+const park = document.querySelector('.park');
+const scoreElement = document.querySelector('.score .value');
 const socket = io();
+
+const treesById = {};
+const dogsById = {};
+
+const petDog = (dog) =>{
+    return (event) =>{
+        socket.emit('pet-dog', {
+            id: dog.id,
+        });
+        if(dogsById[dog.id]) {
+            dogsById[dog.id].remove();
+            delete dogsById[dog.id];
+        }
+    }
+}
+
+
+const updateView = (gameState) =>{
+    scoreElement.textContent = gameState.dogsPet;
+    console.log(gameState);
+    gameState.trees.forEach(tree => { 
+        if(!treesById[tree.id]) {
+            const treeElement = document.createElement('span');
+            treeElement.classList.add('tree');
+            treeElement.textContent = tree.emoji;
+            treesById[tree.id] = treeElement;
+            park.appendChild(treeElement);
+        }
+        treesById[tree.id].style.top = tree.location.y * window.innerHeight + 'px';
+        treesById[tree.id].style.left = tree.location.x * window.innerWidth + 'px';
+    });
+    gameState.dogs.forEach(dog => { 
+        if(!dogsById[dog.id]) {
+            const dogElement = document.createElement('span');
+            dogElement.classList.add('dog');
+            dogElement.classList.add('bounce');
+            dogElement.textContent = dog.emoji;
+            dogsById[dog.id] = dogElement;
+            dogElement.addEventListener('click', petDog(dog));
+            park.appendChild(dogElement);
+        }
+        dogsById[dog.id].style.top = dog.location.y * window.innerHeight + 'px';
+        dogsById[dog.id].style.left = dog.location.x * window.innerWidth + 'px';
+    });
+}
+
+socket.on('game-state', updateView);
--- src/sockets.js
+++ src/sockets.js
@@ -1,10 +1,164 @@
 const socketIO = require('socket.io');
 
+function getRandomPos() {
+    let min = 0.2;
+    let max = 0.85;
+    return Math.random() * (max - min) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
 
 module.exports = (server) =>{
     const io = socketIO(server);
 
+    const gameState = { 
+        dogsPet: 0,
+        trees: [
+            {
+                emoji: '🌲',
+                id: 0,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            }, 
+            {
+                emoji: '🌳',
+                id: 1,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌲',
+                id: 3,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌳',
+                id: 4,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌳',
+                id: 5,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌲',
+                id: 6,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌳',
+                id: 7,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌳',
+                id: 8,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌲',
+                id: 9,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌳',
+                id: 10,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌳',
+                id: 11,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+            {
+                emoji: '🌲',
+                id: 12,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            },
+        ],
+        dogs: [{
+            emoji: '🐶',
+            id: 0,
+            location: {
+                x: getRandomPos(),
+                y: getRandomPos(), 
+            },
+        },
+        {
+            emoji: '🐶',
+            id: 1,
+            location: {
+                x: getRandomPos(),
+                y: getRandomPos(), 
+            },
+        },
+        {
+            emoji: '🐶',
+            id: 2,
+            location: {
+                x: getRandomPos(),
+                y: getRandomPos(), 
+            },
+        },
+    ],
+    };
+
+    let hasUpdate = false
     io.on('connection', (socket) =>{
-        console.log('user connected');
+        socket.emit('game-state', gameState);
+        socket.on('pet-dog', ({ id }) => { 
+            gameState.dogsPet += 1;
+            gameState.dogs = gameState.dogs.filter(dog => dog.id !== id);
+            gameState.dogs.push({
+                emoji: '🐶',
+                id: gameState.dogs[gameState.dogs.length - 1].id + 1,
+                location: {
+                    x: getRandomPos(),
+                    y: getRandomPos(), 
+                },
+            });
+            hasUpdate = true;
+        })
     })
+
+    setInterval(() =>{
+        if(hasUpdate) {
+            io.emit('game-state', gameState);
+            hasUpdate = false;
+        }
+    }, 300);
 }
